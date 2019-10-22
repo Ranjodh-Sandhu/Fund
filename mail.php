328b4afa6bfd3d9f00d93beea95aa187da148eb0
Hoon Kim <jhk4tt@virginia.edu>
2019-10-22 17:22:34 -0400

    Testing the contact us backend

--- mail.php
+++ mail.php
@@ -1,10 +1,28 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "funddteam@gmail.com";
-$subject = $_POST['subject'];
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='form.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+<?php 
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'funddteam@gmail.com';                     // SMTP username
+    $mail->Password   = 'RJLH4753';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    $mail->Subject = $_POST['subject'];
+    $mail->Body    = $_POST['message'];
+
+    $mail->send();
+    echo 'Thank you!';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
