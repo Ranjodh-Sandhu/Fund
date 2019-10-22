6a7edc546d5b2044c5cb8d7fe3a630bc623d80ff
Hoon Kim <jhk4tt@virginia.edu>
2019-10-22 17:30:28 -0400

    Test 2 for contact us

--- mail.php
+++ mail.php
@@ -5,18 +5,17 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'funddteam@gmail.com';                     // SMTP username
     $mail->Password   = 'RJLH4753';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
     $mail->Port       = 587;                                    // TCP port to connect to
 
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->setFrom($_POST['email'], $_POST['name']);
+    //$mail->setFrom($_POST['email'], $_POST['name']);
     $mail->Subject = $_POST['subject'];
     $mail->Body    = $_POST['message'];
 
