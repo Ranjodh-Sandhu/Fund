fca834b1c98a29ddc613114fff6ee48d199e4ec8
Hoon Kim <jhk4tt@virginia.edu>
2019-10-23 11:57:18 -0400

    Contact us page will send email to the business email

--- mail.php
+++ mail.php
@@ -1,14 +1,8 @@
 <?php
       include("sendmail.php");
-      $to       =   "funddteam@gmail.com";
+      $to       =   $_POST['email'];
       $subject  =   $_POST['subject'];
       $message  =   $_POST['message'];
       $name     =   $_POST['name'];
       $mailsend =   sendmail($to,$subject,$message,$name);
-      if($mailsend==1){
-        echo '<h2>Thank You! </h2>';
-      }
-      else{
-        echo '<h2>There are some issue.</h2>';
-      }
 ?>
--- sendmail.php
+++ sendmail.php
@@ -1,31 +1,32 @@
 <?php
-    require_once('class.phpmailer.php');
-    $to = "funddteam@gmail.com";
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $name = $_POST['name'];
+    require_once('PHPMailer-master\src\PHPMailer.php');
+    require_once('PHPMailer-master\src\SMTP.php');
+    require_once('PHPMailer-master\src\Exception.php');
+    use PHPMailer\PHPMailer\PHPMailer;
     function sendmail($to,$subject,$message,$name)
     {
-                  $mail             = new PHPMailer();
-                  $body             = $message;
-                  $mail->IsSMTP();
-                  $mail->SMTPAuth   = true;
-                  $mail->Host       = "smtp.gmail.com";
-                  $mail->Port       = 587;
-                  $mail->Username   = "funddteam@gmail.com";
-                  $mail->Password   = "RJLH4753";
-                  $mail->SMTPSecure = 'tls';
-                  $mail->SetFrom("funddteam@gmail.com", $name);
-                  $mail->AddReplyTo($_POST['email'], $name);
-                  $mail->Subject    = $_POST['subject'];
-                  $mail->AltBody    = "Any message.";
-                  $mail->MsgHTML($body);
-                  $address = $to;
-                  $mail->AddAddress($address, $name);
-                  if(!$mail->Send()) {
-                        echo 'Thank you!';
-                  } else {
-                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                 }
+                  $mail             = new PHPMailer(true);
+                  try{
+                    $body             = "{$name} <br> {$to} <br> {$message}";
+                    $mail->IsSMTP();
+                    $mail->SMTPAuth   = true;
+                    $mail->Host       = "smtp.gmail.com";
+                    $mail->Port       = 587;
+                    $mail->Username   = "funddteam@gmail.com";
+                    $mail->Password   = "RJLH4753";
+                    $mail->SMTPSecure = 'tls';
+                    $mail->SetFrom('funddteam@gmail.com', 'Fundd team');
+                    $mail->AddReplyTo("funddteam@gmail.com", "Fundd team");
+                    $mail->Subject    = "Contact Us: {$subject}";
+                    $mail->AltBody    = "Any message.";
+                    $mail->MsgHTML($body);
+                    $address = "funddteam@gmail.com";
+                    $mail->AddAddress($address, $name);
+                    $mail->send();
+                    echo '<h2>Thank You!</h2>';
+                  }catch(Exception $e) {
+                      echo "Mail cannot be sent: {$mail->ErrorInfo}";
+                  }
+                  
     }
 ?>
