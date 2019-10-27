54a90ca7b1cbd1967f3b929caaf2e647833a67a7
Ranjodh <rss6py@virginia.edu>
2019-10-27 18:57:20 -0400

    Tied everything together

---
+++ ipn.php
@@ -0,0 +1,85 @@
+<?php
+/*
+This IPN url is set when creating an invoice, modify as needed to integrate with your system
+This also uses the EXTENDED notifications, the preferred way to receive IPN updates on every status of your invoice
+https://bitpay.com/api 
+*/
+/*
+Autoload the classes
+ */
+
+function BPC_autoloader($class)
+{
+    #change the pathing if needed
+    if (strpos($class, 'BPC_') !== false):
+        if (!class_exists('/BitPayLib/' . $class, false)):
+            #doesnt exist so include it
+            include '/BitPayLib/' . $class . '.php';
+        endif;
+    endif;
+}
+spl_autoload_register('BPC_autoloader');
+
+
+$all_data = json_decode(file_get_contents("php://input"), true);
+
+$data = $all_data['data'];
+$event = $all_data['event'];
+$orderId = $data['orderId'];
+$invoiceID = $data['id'];
+$transaction_status = $event['name'];
+
+/* We recommend double checking the invoice data by calling the Invoice API endpoint, instead of relying 100% on incoming notifications */
+
+$bitpay_checkout_token = '5wPEf37FBeHX2gEbPSctcsHB2amXkJZTVTrdvN9WrgKc';
+$environment = 'test';
+
+/*Create your configuration object, passing the $environment of 'test' or 'production'*/
+$config = new BPC_Configuration($bitpay_checkout_token, $environment);
+
+/*Start building your parameters object to send to the API, filling in the details with your own data*/
+$params = new stdClass();
+
+$params->invoiceID = $invoiceID;
+
+$item = new BPC_Item($config, $params);
+
+$invoice = new BPC_Invoice($item); //this creates the invoice with all of the config params
+$orderStatus = json_decode($invoice->BPC_checkInvoiceStatus($invoiceID));
+
+switch ($transaction_status) {
+    case 'invoice_confirmed':
+    if ($orderStatus->data->status == 'confirmed'):
+        /* The invoice has gone through the confirmation and is complete */
+        /* Do your custom integration here */   
+    endif;
+    break;
+
+    case 'invoice_paidInFull':
+    if ($orderStatus->data->status == 'paid'):
+        /* The default state when a user pays for an invoice but has not confirmed */
+        /* Do your custom integration here */   
+    endif;
+    break;
+
+    case 'invoice_failedToConfirm':
+    if ($orderStatus->data->status == 'invalid'):
+        /* There's an error, most likely network congestion, so update any order access */
+        /* Do your custom integration here */   
+    endif;
+    break;
+
+    case 'invoice_expired':
+    if ($orderStatus->data->status == 'expired'):
+        /* An invoice was created but never paid, default is 15 minutes.  Many users delete the order and access in their system at this state */
+        /* Do your custom integration here */   
+    endif;
+    break;
+
+    case 'invoice_refundComplete':
+    if ($orderStatus->data->status == 'refunded'):
+        /* Invoice is refunded */
+        /* Do your custom integration here */   
+    endif;
+    break;
+}
---
+++ php-bitpay-client-lite/README.md
@@ -0,0 +1,43 @@
+bitpay/php-bitpay-client
+=================
+
+# Requirements
+You will need an API token from <https://test.bitpay.com/dashboard/merchant/api-tokens> or <https://bitpay.com/dashboard/merchant/api-tokens>
+
+You will also need to complete the account setup from <https://test.bitpay.com/dashboard/signup> or <https://bitpay.com/dashboard/signup>
+
+After you have finished the setup, follow the example in **invoice.php** to connect to the API and start receiving payments through BitPay
+
+***note:*** *New accounts are set to a Tier 0, with a $500 daily limit.  To receive more, you will need to upgrade your account to the different Tier levels (all free) and provide documentation as needed*
+
+
+# Support
+
+* https://github.com/bitpay/php-bitpay-client/issues
+* https://support.bitpay.com
+
+When you receive blank IPN responses, please check https://support.bitpay.com/hc/en-us/articles/115003025706-Why-am-I-getting-a-blank-IPN-post-response-from-BitPay-when-using-PHP-
+
+# License
+
+The MIT License (MIT)
+
+Copyright (c) 2018 BitPay, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
